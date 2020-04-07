9e0c9016e0264c03d6004fedc2565b1cf49eaaa4
Sivadej K <bombyk@gmail.com>
2020-04-07 11:46:54 -0800

    math!

--- app.js
+++ app.js
@@ -4,22 +4,38 @@ const app = express();
 app.use(express.json());
 
 app.get('/mean/:nums', (req, res) => {
-	let nums = req.params.nums.split(',');
-	nums = nums.map(n => parseInt(n));
-	let sum = nums.reduce((sum, num) => {
-		return sum + num;
-	}, 0);
+	let nums = getArrayOfNums(req.params.nums,',');
+	let sum = nums.reduce((sum, num) => { return sum + num }, 0);
 	let mean = sum / nums.length;
 	return res.send(`${mean}`);
 })
 
-app.get('/median', (req, res) => {
-	return '';
+app.get('/median/:nums', (req, res) => {
+	const nums = getArrayOfNums(req.params.nums,',');
+	return res.send(`${median(nums)}`);
 })
 
-app.get('/mode', (req, res) => {
-	return '';
+app.get('/mode/:nums', (req, res) => {
+	const nums = getArrayOfNums(req.params.nums,',');
+	return res.send(`${mode(nums)}`);
 })
+
+const getArrayOfNums = (strNums,delimiter) => {
+	let numsArr = strNums.split(delimiter);
+	return numsArr.map(n => parseInt(n));
+}
+
+const median = arr => {
+	const mid = Math.floor(arr.length/2);
+	const nums = arr.sort((a,b)=>a-b);
+	return arr.length % 2 !== 0 ? nums[mid] : (nums[mid-1]+nums[mid]/2);
+};
+
+const mode = arr => {
+	// mode: number that occurs the most often.
+	// should be able to return multiple values
+	return null;
+};
 
 app.listen(3000, () => {
 	console.log('app running on port 3000');
